f2d0c40572400410506011b9896011d53a3931e4
Chris Carpentier <coredump@gmail.com>
2019-10-17 14:51:40 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+RxPerl - Blah blah blah
+
+# SYNOPSIS
+
+    use RxPerl;
+
+# DESCRIPTION
+
+RxPerl is
+
+# AUTHOR
+
+Chris Carpentier <coredump@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2019- Chris Carpentier
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/RxPerl.pm
@@ -0,0 +1,39 @@
+package RxPerl;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+RxPerl - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use RxPerl;
+
+=head1 DESCRIPTION
+
+RxPerl is
+
+=head1 AUTHOR
+
+Chris Carpentier E<lt>coredump@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2019- Chris Carpentier
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
